887878fd706d9cf640764b068cd81ee4fdb17b3d
Ivancho <ivanfer5_25@hotmail.com>
2014-10-23 16:15:31 -0500

    Comentar Foro

--- LUS/LUS/static/js/consulta_editor.js
+++ LUS/LUS/static/js/consulta_editor.js
@@ -2,4 +2,6 @@ jQuery(document).ready(function($) {
     $( 'textarea#id_contenido' ).ckeditor();
     $( 'textarea#id_animacion' ).ckeditor();
     $( 'textarea#id_texto' ).ckeditor();
+    // ALLOW <i></i>
+    config.protectedSource.push(/<i[^>]*><\/i>/g);
 } );
